3983bcf874b46f37ae82123b737b54fd873ce6b8
thriqon <github@jonasw.de>
2015-08-06 16:26:26 +0200

    testing more ember versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -32,13 +32,49 @@ module.exports = {
 		}
 	},
 	{
-		name: 'ember-1-12',
-		dependencies: {
-			'ember': 'components/ember#1.12.1'
-		},
-		resolutions: {
-			'ember': 'release'
-		}
+		name: 'ember-1-12-0',
+		dependencies: {'ember': 'components/ember#1.12.0'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-12-1',
+		dependencies: {'ember': 'components/ember#1.12.1'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-0',
+		dependencies: {'ember': 'components/ember#1.13.0'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-1',
+		dependencies: {'ember': 'components/ember#1.13.1'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-2',
+		dependencies: {'ember': 'components/ember#1.13.2'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-3',
+		dependencies: {'ember': 'components/ember#1.13.3'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-4',
+		dependencies: {'ember': 'components/ember#1.13.4'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-5',
+		dependencies: {'ember': 'components/ember#1.13.5'},
+		resolutions: {'ember': 'release'}
+	},
+	{
+		name: 'ember-1-13-6',
+		dependencies: {'ember': 'components/ember#1.13.6'},
+		resolutions: {'ember': 'release'}
 	}
 	]
 };
