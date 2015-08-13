85fbaf8bbd6ff6cb2781c365acf16d20f8d14c6c
Jonas Weber <github@jonasw.de>
2015-08-13 21:18:41 +0200

    supporting Ember 2

--- config/ember-try.js
+++ config/ember-try.js
@@ -33,48 +33,44 @@ module.exports = {
 	},
 	{
 		name: 'ember-1-12-0',
-		dependencies: {'ember': 'components/ember#1.12.0'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.12.0'}
 	},
 	{
 		name: 'ember-1-12-1',
-		dependencies: {'ember': 'components/ember#1.12.1'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.12.1'}
 	},
 	{
 		name: 'ember-1-13-0',
-		dependencies: {'ember': 'components/ember#1.13.0'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.0'}
 	},
 	{
 		name: 'ember-1-13-1',
-		dependencies: {'ember': 'components/ember#1.13.1'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.1'}
 	},
 	{
 		name: 'ember-1-13-2',
-		dependencies: {'ember': 'components/ember#1.13.2'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.2'}
 	},
 	{
 		name: 'ember-1-13-3',
-		dependencies: {'ember': 'components/ember#1.13.3'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.3'}
 	},
 	{
 		name: 'ember-1-13-4',
-		dependencies: {'ember': 'components/ember#1.13.4'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.4'}
 	},
 	{
 		name: 'ember-1-13-5',
-		dependencies: {'ember': 'components/ember#1.13.5'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.5'}
 	},
 	{
 		name: 'ember-1-13-6',
-		dependencies: {'ember': 'components/ember#1.13.6'},
-		resolutions: {'ember': 'release'}
+		dependencies: {'ember': 'components/ember#1.13.6'}
+	},
+	{
+		name: 'ember-2-0-0',
+		dependencies: {'ember': 'components/ember#2.0.0'},
+		resolutions: { 'ember': '2.0.0' }
 	}
 	]
 };
